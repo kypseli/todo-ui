e9ddb05f66ec5762bb1cad76bcb4ee4b69d1aeca
Beedemo UI <37445588+beedemo-ui@users.noreply.github.com>
2018-08-23 19:40:35 -0400

    Update init.test.js
    Update page url

--- tests/init.test.js
+++ tests/init.test.js
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 
 
 fixture `Initial page`
-   .page `http://127.0.0.1:8000`;
+   .page `localhost`;
 
 
 test('Check new-todo placeholder text and new input', async t => {
