d65dbfa4a721ad41825f9606897df04192f199ae
Beedemo UI <beedemo.ui@gmail.com>
2018-09-11 03:52:33 +0000

    attempt to fix test

--- tests/init.test.js
+++ tests/init.test.js
@@ -7,7 +7,7 @@ fixture `Initial page`
 
 test('Check new-todo placeholder text and new input', async t => {
    const newToDoInput = Selector('input.new-todo');
-   const addedToDo = Selector('.todo-list li label')
+   const addedToDo = Selector('.todo-list > li > label')
 
    await t
        .expect(newToDoInput.value).eql('', 'input has no value')
